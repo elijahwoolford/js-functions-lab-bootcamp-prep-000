57d3e4a2735414a21cb8ae1392cabcb561914b6b
Elijah Woolford <elijah.woolford@gmail.com>
2018-01-15 00:06:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ function happyHolidays(){
 function happyHolidaysTo(holiday, person){
   return `Happy ${holiday}, ${person}!`;
 }
+
+function holidayCountdown(holiday, days){
+  return `It's ${days} days until ${holiday}!`;
+}
